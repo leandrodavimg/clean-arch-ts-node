d58b5ec1983b0c76609931431efffc80a54401e9
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-07 14:13:29 -0300

    feat: create file to orchestrate connections

---
+++ app/use-cases/create-user/index.ts
@@ -0,0 +1,17 @@
+import { SendMailMailtrap } from "../../infra/providers/mailtrap/send-mail-mailtrap";
+import { PostgresFake } from "../../infra/repositories/postgress-fake/postgress-fake";
+import { CreateuserController } from "./create-user-controller";
+import { CreateUserUseCase } from "./create-user-use-case";
+
+const postgreFakeRepository = new PostgresFake()
+const mailtrapMailProvider = new SendMailMailtrap()
+
+const createUserUseCase = new CreateUserUseCase(
+  postgreFakeRepository, mailtrapMailProvider
+)
+
+const createUserController = new CreateuserController(
+  createUserUseCase
+)
+
+export {createUserUseCase, createUserController}

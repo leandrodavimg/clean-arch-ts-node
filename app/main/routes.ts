9e41a2084315eb02c88bda98d85b2ac205b7f7f8
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-07 14:28:29 -0300

    feat: remove coment

--- app/main/routes.ts
+++ app/main/routes.ts
@@ -4,7 +4,6 @@ import { createUserController } from "../use-cases/create-user";
 const router = Router()
 
 router.post('/users', (req: Request, res: Response) => {
-  // return res.send(req.body)
   return createUserController.handle(req, res)
 })
 

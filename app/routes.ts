3b3c0c5cdc6418773db35addb9522cee5c487efc
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-07 14:22:27 -0300

    refactor: is this

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,8 @@ import { createUserController } from "./use-cases/create-user";
 
 const router = Router()
 
-router.post('/users', (res: Response, req: Request) => {
+router.post('/users', (req: Request, res: Response) => {
+  // return res.send(req.body)
   return createUserController.handle(req, res)
 })
 

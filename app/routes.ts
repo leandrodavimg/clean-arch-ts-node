fb819215911f99e7c0668ea3c98579576f1c687d
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-07 14:13:47 -0300

    feat: add controller in routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from "express";
+import { createUserController } from "./use-cases/create-user";
 
 const router = Router()
 
 router.post('/users', (res: Response, req: Request) => {
-  return res.status(200).send('Welcome in my app')
+  return createUserController.handle(req, res)
 })
 
 export { router }

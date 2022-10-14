38043c28497fd3ab8dd50db7024c1addf0752884
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-13 23:18:58 -0300

    feat: configure server to typeorm

--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,20 @@
 import { app } from './main/app'
-import { port } from './config/env'
+import { AppDataSource } from './config/connection/typeorm'
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
+// import { AppDataSourceMongo } from './config/connection/mongoose'
+// import '../config/connection/mongoose'
+
+// iniciando o express com o typeorm
+AppDataSource.initialize().then(() => {
+  console.log('TYPEORM INIT')
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
+  })
 })
+
+
+
+// Iniciando a conexao com o mongo
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running at http://localhost:${process.env.PORT}`)
+// })

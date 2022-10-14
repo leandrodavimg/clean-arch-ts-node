2ab810f0546c08bffa18ac883f1b9be164cd25f9
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-13 23:30:47 -0300

    feat: change conect mongoose

--- app/config/connection/mongoose.ts
+++ app/config/connection/mongoose.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 
-mongoose.connect(process.env.MONGO_DB, () => {
-  console.log('Conected mongodb with mongoose')
+export const AppMongoose = mongoose.connect(process.env.MONGO_DB).then(() => {
+  console.log('Mongo connected')
 })
-
+.catch(err => {
+    console.log('Failed to connect to MongoDB', err)
+  })

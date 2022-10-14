139df6a6b4766155c27de591f25138fc1bc07361
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-14 00:04:36 -0300

    feat: change text show start

--- app/server.ts
+++ app/server.ts
@@ -6,13 +6,13 @@ import { AppMongoose } from './config/connection/mongoose'
 // Start app with typeorm
 // AppDataSource.initialize().then(() => {
 //   app.listen(process.env.PORT, () => {
-//     console.log(`Server running at http://localhost:${process.env.PORT}`)
+//     console.log(`Server running at http://localhost:${process.env.PORT} with typeorm`)
 //   })
 // })
 
 // start app with mongoose
 AppMongoose.then(() => {
   app.listen(process.env.PORT, () => {
-    console.log(`Server running at http://localhost:${process.env.PORT}`)
+    console.log(`Server running at http://localhost:${process.env.PORT} with mongoose`)
   })
 })

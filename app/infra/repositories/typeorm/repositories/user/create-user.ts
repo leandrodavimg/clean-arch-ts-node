8ca5019d74de6ed2fb07fe8bbd3c399528b5df36
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-15 13:31:38 -0300

    feat: implement findById in repository

--- app/infra/repositories/typeorm/repositories/user/create-user.ts
+++ app/infra/repositories/typeorm/repositories/user/create-user.ts
@@ -8,14 +8,20 @@ export class UserBD implements IUserRepository {
   constructor() {
     this.repository = AppDataSource.getRepository(User)
   }
+  
   async save(user: IUser): Promise<IUser> {
     const createUser = this.repository.create(user)
     await this.repository.save(createUser)
     return createUser
   }
 
-  async findByEmail(emial: string): Promise<boolean> {
+  async findByEmail(emial: string): Promise<IUser> {
       const user = await this.repository.findOneBy({email: emial})
-      return user ? true : false
+      return user
+  }
+
+  async findById(id: string): Promise<IUser> {
+      const user = await this.repository.findOneBy({ id: id })
+      return user
   }
 }

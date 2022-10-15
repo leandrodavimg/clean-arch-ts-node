e864bbbfe9510589b64a33f748d01a205d048879
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-15 13:32:06 -0300

    refactor: remove promisse when not need

--- app/providers/encript-provider.ts
+++ app/providers/encript-provider.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/user/User"
 
 export interface IBcrypt {
-  hash(password: string): Promise<string>
+  hash(password: string): string
   compare(password: string, user: User): Promise<boolean>
 }

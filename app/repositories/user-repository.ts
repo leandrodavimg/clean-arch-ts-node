f5576b6a44cdc913725de00124340e56de7f51e4
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-15 13:32:19 -0300

    feat: implement findById in repository

--- app/repositories/user-repository.ts
+++ app/repositories/user-repository.ts
@@ -1,6 +1,7 @@
 import { User } from "../entities/user/User";
 
 export interface IUserRepository {
-  findByEmail(emial: string): Promise<boolean>
+  findByEmail(emial: string): Promise<User>
+  findById(id: string): Promise<User>
   save(user: User): Promise<User>
 }

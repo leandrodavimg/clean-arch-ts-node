c1cb1b035421e704b246755bb9ecc0cc19e79dc6
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-24 21:33:42 -0300

    feat: resolvind path

--- app/infra/providers/encript/bcrypt.ts
+++ app/infra/providers/encript/bcrypt.ts
@@ -1,6 +1,6 @@
-import { IBcrypt } from "../../../providers/encript-provider";
+import { IBcrypt } from "@providers/encript-provider";
 import bcrypt from 'bcrypt'
-import { User } from "../../../entities/user/User";
+import { User } from "@entities/user/User";
 
 export class BcryptEncode implements IBcrypt {
   constructor() {

62c57d4d046cc7754a837aa64aee0f8c1d809ab7
Leandro Freitas <leandrodavimg@gmail.com>
2022-10-26 21:57:45 -0300

    feat: add cors

--- src/main/app/index.ts
+++ src/main/app/index.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 import express from 'express'
 import swaggerUI from 'swagger-ui-express'
@@ -8,7 +9,29 @@ import swaggerDocs from '@config/swagger/swagger.json'
 const app = express()
 
 app.use(express.json())
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs)) //refatorar
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs)) // ! refactor swagger
+
+// ! refactor cors
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: '',
+  origin: ['http://localhost:4444'],
+  preflightContinue: false
+}
+
+// Use with middleware
+router.use(cors(options))
+// Enable pre-flight
+router.options('*', cors(options))
+
+
 app.use(router)
 
 
